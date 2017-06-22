20e5d8c703cf04cdec97a80dc93e3ea57ec9a3e2
jack <17764576259@163.com>
2017-06-22 17:11:12 +0800

    新增Test

---
+++ app/src/main/java/cn/share/jack/mvpmasterdemo/ui/TestActivity.java
@@ -0,0 +1,20 @@
+package cn.share.jack.mvpmasterdemo.ui;
+
+import cn.share.jack.mvpmasterdemo.R;
+import cn.share.jack.mvpmasterdemo.base.BaseActivity;
+
+/**
+ * Created by jack on 2017/6/21
+ */
+
+public class TestActivity extends BaseActivity {
+    @Override
+    protected int layoutRes() {
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
